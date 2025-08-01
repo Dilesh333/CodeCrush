7ad84195a824ece40ddd6c906ff5666ea147600e
Dilesh333 <dilesh.dev333@gmail.com>
2025-08-01 12:51:22 +0530

    first data saved to db

--- src/app.js
+++ src/app.js
@@ -2,6 +2,26 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const port = 3000;
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Virat",
+    lastName: "Kohli",
+    emailId: "virat@gmail.com",
+    password: "Virat@123",
+  });
+
+  
+  try{
+    await user.save();
+    res.send("User Added Successfully");
+  }
+  catch(err){
+    res.status(400).send("Error adding user: " + err.message);
+  }
+  
+});
 
 connectDB()
   .then(() => {
