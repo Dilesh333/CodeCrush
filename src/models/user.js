76992a26bf194f9e550a2a02289dc7bcdbaa0704
Dilesh333 <dilesh.dev333@gmail.com>
2025-08-05 11:04:26 +0530

    validator library added

--- src/models/user.js
+++ src/models/user.js
@@ -1,55 +1,72 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minlength: 4,
-    maxlength: 20,
-  },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    lowercase: true,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    min: 18,
-  },
-  gender: {
-    type: String,
-    validate(value) {
-      if (!["male", "female", "others"].includes(value)) {
-        throw new Error("Gender data is not valid");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 4,
+      maxlength: 20,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address:" + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Your Password is weak:" + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photUrl: {
+      type: String,
+      default:
+        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
+        validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL:" + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "Hey there! I am using DevCrush",
+    },
+    skills: {
+      type: [String],
     },
   },
-  photUrl: {
-    type: String,
-    default:
-      "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
-  },
-  about: {
-    type: String,
-    default: "Hey there! I am using DevCrush",
-  },
-  skills: {
-    type: [String],
-  },
-},
-{
+  {
     timestamps: true,
-})
-
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
