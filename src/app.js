de63e4e181a66095e1ad73d0271381e33cc4cde8
Dilesh333 <dilesh.dev333@gmail.com>
2025-08-05 10:46:09 +0530

    API level validation for patch API

--- src/app.js
+++ src/app.js
@@ -46,30 +46,44 @@ app.get("/feed", async (req, res) => {
 });
 
 //delete user by userId
-app.delete("/user", async(req,res)=>{
-  const userId = req.body.userId
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
 
   try {
-    const user = await User.findByIdAndDelete(userId)
-    res.send("User Deleted Sucessfully!")
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User Deleted Sucessfully!");
   } catch (error) {
-    res.status(400).send("Something went wrong")
+    res.status(400).send("Something went wrong");
   }
-})
+});
 
 //update the userbyID
-app.patch("/user", async(req,res)=>{
-  const userId = req.body.userId
-  const data = req.body
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
 
   try {
-    const user = await User.findByIdAndUpdate({_id: userId}, data)
-    res.send("User updated successfuly!")
+    const ALLOWED_UPDATES = ["photoUrl", "About", "skills"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+
+    if(data.skills.length>10){
+      throw new Error("Skills cannot be more than 10")
+    }
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
+    res.send("User updated successfuly!");
   } catch (error) {
-     res.status(400).send("Something went wrong")
+    res.status(400).send("Update failed: " + error.message);
   }
-  
-})
+});
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,55 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName:{
-        type: String,
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 20,
+  },
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+    lowercase: true,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
+  gender: {
+    type: String,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("Gender data is not valid");
+      }
     },
-    lastName:{
-        type: String,
-    },
-    emailId:{
-        type: String
-    },
-    password:{
-        type: String
-    },
-    age: {
-        type: Number
-    },
-    gender:{
-        type: String
-    }
+  },
+  photUrl: {
+    type: String,
+    default:
+      "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
+  },
+  about: {
+    type: String,
+    default: "Hey there! I am using DevCrush",
+  },
+  skills: {
+    type: [String],
+  },
+},
+{
+    timestamps: true,
 })
+
 
 const User = mongoose.model("User", userSchema);
 
