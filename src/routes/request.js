47fa97ecaef6dff3d7775edd6b3f8264a088256a
Dilesh333 <dilesh.dev333@gmail.com>
2025-08-11 09:36:48 +0530

    fix- connection req already exists and if user not added

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,6 +2,7 @@ const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 requestRouter.post(
   "/request/send/:status/:toUserId",
@@ -12,10 +13,29 @@ requestRouter.post(
       const toUserId = req.params.toUserId;
       const status = req.params.status;
 
-      const allowedStatus = ["interested", "ignored"]
+      const allowedStatus = ["interested", "ignored"];
 
-      if(!allowedStatus.includes(status)){
-        throw new Error("Invalid status type")
+      if (!allowedStatus.includes(status)) {
+        throw new Error("Invalid status type");
+      }
+
+      //if user not added
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      //if connectionRequest already exist
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection Request Already Exists!!" });
       }
 
       const connectionRequest = new ConnectionRequest({
